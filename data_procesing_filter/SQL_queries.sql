446fdd77cc2da713406197a0ea4b450b08abf54d
sebastianjinich <sjinich@estudiantes.unsam.edu.ar>
2023-05-19 10:27:07 -0300

    arreglando cosas para pasar al server

--- data_procesing_filter/SQL_queries.sql
+++ data_procesing_filter/SQL_queries.sql
@@ -24,31 +24,35 @@ WHERE (trgd.target_type = 'SINGLE PROTEIN')
 LIMIT 10
 ;
 
-
+-- QUERY posta!
 
 SELECT act.molregno, 
-        md.chembl_id AS compund_chembl_id,
-        trgd.chembl_id AS target_chemb_id,
-        act.standard_relation,
-        act.standard_value AS bioactivity,
-        act.standard_units,
-        act.standard_type,
-        act.potential_duplicate,
-        cmpstc.canonical_smiles AS smiles,
-        cs.sequence AS target_sequence
-INTO OUTFILE '/var/lib/mysql-files/data_final_con_seq.csv'
+    md.chembl_id AS comp_id,
+    ass.tid as tid,
+    trgd.chembl_id AS target,
+    act.standard_relation AS relation,
+    act.standard_value AS bioactivity,
+    act.standard_units AS units,
+    act.standard_type AS type,
+    act.potential_duplicate,
+    act.pchembl_value,
+    trgd.organism,
+    trgd.target_type,
+    ass.assay_type,
+    cmpstc.canonical_smiles AS smiles,
+    cmpstc.standard_inchi_key AS inchi_key,
+    cs.sequence AS sequence
+INTO OUTFILE '/var/lib/mysql-files/data_chembl25_13_04_23.csv'
 FROM activities AS act
-JOIN (SELECT tid, assay_id, assay_type, chembl_id FROM assays) AS ass ON act.assay_id = ass.assay_id
+JOIN assays AS ass ON act.assay_id = ass.assay_id
 JOIN target_dictionary AS trgd ON ass.tid = trgd.tid
 JOIN compound_structures AS cmpstc ON act.molregno = cmpstc.molregno
 JOIN molecule_dictionary AS md ON act.molregno = md.molregno
 JOIN target_components AS tc ON ass.tid = tc.tid
 JOIN component_sequences AS cs ON tc.component_id = cs.component_id
-WHERE (trgd.target_type = 'SINGLE PROTEIN')
-    AND (ass.assay_type ='B')
-    AND (act.standard_type IN ('IC50','EC50','AC50','Ki','Kd','Potency'))
-    AND (act.pchembl_value IS NOT NULL)
 ;
+
+
 
 -- query para las smiles
 
